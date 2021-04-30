24bd90022296fcc9c8119a73b35625cdd580f675
Brian Henry <BrianHenryIE@gmail.com>
2021-04-30 15:51:58 -0700

    strauss

--- src/autoload.php
+++ src/autoload.php
@@ -15,7 +15,7 @@ namespace BrianHenryIE\WC_CSP_Condition_Customer;
 
 use BrianHenryIE\WC_CSP_Condition_Customer\Pablo_Pacheco\WP_Namespace_Autoloader\WP_Namespace_Autoloader;
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'strauss' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $wpcs_autoloader = new WP_Namespace_Autoloader();
 $wpcs_autoloader->init();
